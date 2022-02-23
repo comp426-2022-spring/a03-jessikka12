aedc382f90724403bc6ef9c29a95cffbf3508b73
Jessica Wang <jessikka@email.unc.edu>
2022-02-23 17:38:44 -0500

    args and start app server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 // require things
 const express = require('express')
 const app = express()
-const minimist = require ('minimist')
 
+const args = require('minimist')(process.argv.slice(2))
+args["port"]
+const port = args.port || 200
+
+// start app server
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace("%PORT%", port))
+})
